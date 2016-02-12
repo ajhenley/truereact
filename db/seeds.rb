06929e878f9f67813cdb18eba4a16fce6e61e343
AJ Henley <ajhenley@gmail.com>
2016-02-12 22:05:33 +0000

    fixed name of data file in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ CSV.foreach(file, :headers => true, :encoding => 'windows-1251:utf-8') do |row|
   Survey.where(:crn => crn).update_all(crsenum: crsenum)
 end
 
-file = Rails.root + "app/assets/data6.csv"
+file = Rails.root + "app/assets/data7.csv"
 CSV.foreach(file, :headers => true, :encoding => 'windows-1251:utf-8') do |row|
   theid = row["INSTRUCTOR_ID"]
   email = row["emails"]
